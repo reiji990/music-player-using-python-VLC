fcb2ef0fce83f6936910435e38ba9c143d812401
reiji990 <101491438+reiji990@users.noreply.github.com>
2023-01-15 02:29:27 +0900

    2023/01/14
    アーティスト名、アルバム名を入力するGUI作成
    再生用GUI途中。

---
+++ play.py
@@ -0,0 +1,150 @@
+# 再生するアーティスト名、アルバム名の一部を入力すると、　フォルダ内の*.flacファイルを再生リスト化しループ再生開始
+# その他基本的な音楽再生アプリの機能（e.g. 一時停止、トラックの変更）が行える
+
+import vlc
+import glob
+import os
+import gc
+
+#カレントディレクトリをこのファイルの絶対パスに変更
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+print("*** start ***")
+
+# *.flacファイルを再生リスト化
+path = open('path.txt', mode='r')
+data = path.read()
+path.close
+print(data)
+
+glflac = sorted(glob.glob(data))
+l = vlc.MediaList(glflac)
+
+# 再生アルバム一覧
+print("\nアルバム一覧")
+allfoldername = [os.path.dirname(r) for r in glflac]
+
+foldername = []
+for element in allfoldername:
+    if element not in foldername:
+        foldername.append(element)
+
+i = 0
+while i < len(foldername):
+    print(f"{i+1}. {(foldername)[i]}")
+    i += 1
+
+# 再生楽曲一覧
+print("\nプレイリスト")
+filename = [os.path.basename(r) for r in glflac]
+
+i = 0
+while i < len(glflac):
+    print(f"{i+1}. {(filename)[i]}")
+    i += 1
+
+# メディアプレイヤーオブジェクトの作成、再生
+p = vlc.MediaListPlayer()
+p.set_media_list(l)
+p.set_playback_mode(vlc.PlaybackMode.loop)
+p.play()
+
+# メモリ解放
+gc.collect()
+
+# プレイヤーウィンドウ表示
+import tkinter
+from tkinter import ttk
+root = tkinter.Tk()
+root.title(u"music player")
+
+root.option_add('*font', ('', 30))
+root.config(bg='black') 
+root.option_add('*Button.background', 'gray')
+root.option_add('*Entry.background','gray')
+root.resizable(0,0)
+
+static = tkinter.Label(text=u'Music Player for VLC',bg='black',fg='white',font=("",50))
+static.grid(columnspan=3)
+
+# input number to select track
+selectnum=tkinter.Entry(root, width=15)
+selectnum.grid(row=2,column=0)
+
+# imfo label
+imfolabel=tkinter.Label(root,text=u'imfomation',bg='black',fg='white')
+imfolabel.grid(row=0,columnspan=2)
+
+# 操作ボタンの機能の関数
+def btnpauseclick():
+    p.pause()
+
+def btnimfoclick():
+    x = p.get_media_player().get_media()
+    index = l.index_of_item(x)
+    print(f"now playing: {index+1}. {filename[index]}\n")
+
+def btnnextclick():
+    p.next()
+
+    x = p.get_media_player().get_media()
+    index = l.index_of_item(x)
+    print(f"next: {filename[index]}")
+
+def btnbackclick():
+    p.previous()
+
+    x = p.get_media_player().get_media()
+    index = l.index_of_item(x)
+    print(f"next: {filename[index]}")
+
+def btnselectclick():
+    print("slect the track number")
+    track = int(input("> "))
+    p.play_item_at_index(track-1)  
+
+    x = p.get_media_player().get_media()
+    index = l.index_of_item(x)
+    print(f"next: {filename[index]}")
+
+def btnquitclick():
+    p.stop()
+    del p
+    del l
+    del glflac
+    gc.collect()
+
+# 操作ボタンの追加
+btnpause=tkinter.Button(root, text="Pause or Play", 
+    command=btnpauseclick)
+btnpause.grid(row=3,columnspan=1)
+
+btnimfo=tkinter.Button(root, text="Imfomation", 
+    command=btnimfoclick)
+btnpause.grid(row=0,colum=0)
+
+btnnext=tkinter.Button(root, text="Next track", 
+    command=btnnextclick)
+btnpause.grid(row=1,colum=0)
+
+btnback=tkinter.Button(root, text="Back track", 
+    command=btnbackclick)
+btnpause.grid(row=1,colum=1)
+
+btnselect=tkinter.Button(root, text="Select track", 
+    command=btnselectclick)
+btnpause.grid(row=2,colum=1)
+
+btnquit=tkinter.Button(root, text="Quit", 
+    command=btnquitclick)
+btnpause.grid(row=3,colum=2)
+
+
+
+# メモリの解放
+del p
+del l
+del glflac
+gc.collect()
+
+print("*** end ***") 
---
+++ pythonGUI.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+import sys
+import subprocess
+import os
+import gc
+import tkinter
+from tkinter import ttk
+
+# カレントディレクトリをこのファイルの絶対パスに変更
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+# GUIアプリのデザイン
+root = tkinter.Tk()
+root.title(u"music player")
+
+root.option_add('*font', ('', 30))
+root.config(bg='black') 
+root.option_add('*Button.background', 'gray')
+root.option_add('*Entry.background','gray')
+root.resizable(0,0)
+
+static = tkinter.Label(text=u'Music Player for VLC',bg='black',fg='white',font=("",50))
+static.grid(columnspan=3)
+
+
+# 入力欄
+artist = tkinter.Label(root, text=u'artist',bg='black',fg='white')
+artist.grid(row=2,column=0)
+
+album = tkinter.Label(root, text=u'album',bg='black',fg='white')
+album.grid(row=3,column=0)
+
+text1=tkinter.Entry(root, width=30)
+text1.grid(row=2,column=1)
+
+text2=tkinter.Entry(root, width=30)
+text2.grid(row=3,column=1)
+
+# pass.txtにリンク作成、別ファイルでpass.txtのリンクを取得、再生開始する関数(playボタンで実行)
+def btnclick():
+    text1in = str(text1.get())
+    text2in = str(text2.get())
+    path = open('path.txt', mode='w', encoding='utf-8')
+    path.write('/System/Volumes/Data/Volumes/*/Music/*' + text1in + '*/*' + text2in + '*/*.flac' )
+    path.close()
+    import play
+
+# playボタン
+btnRead=tkinter.Button(root, text="Play", 
+    command=btnclick)
+btnRead.grid(row=5,columnspan=3)
+
+
+gc.collect()
+root.mainloop()
